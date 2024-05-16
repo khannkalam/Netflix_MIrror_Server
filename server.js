1622492330c8b8dfdea1a9320b9d6766ca4c8405
kalam khann <khannkalam@gmail.com>
2024-05-16 20:29:33 +0530

    backend done

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb+srv://khannkalam:ebrsh7cW7nRl1f1G@cluster07.cbgbfga.mongodb.net/?retryWrites=true&w=majority&appName=cluster07/netflix_mirror", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
